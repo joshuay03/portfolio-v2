6425ef6f70045a2f4502af76b39e396f54224c4c
Joshua Young <djry1999@gmail.com>
2021-06-22 22:18:32 +1000

    Configure colors and fonts

--- portfolio/tailwind.config.js
+++ portfolio/tailwind.config.js
@@ -1,11 +1,25 @@
 module.exports = {
-  purge: ['./public/index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
-  darkMode: 'media', // or 'media' or 'class'
+  purge: ["./public/index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
+  darkMode: "media", // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'synth-blue': '#2ee2fa',
+        'synth-gold': '#ff8b39',
+        'synth-green': '#70f1b9',
+        'synth-grey': '#bbbbbb',
+        'synth-pink': '#f92aad',
+        'synth-purple-light': '#2a2139',
+        'synth-purple-mid': '#241b2f',
+        'synth-purple-dark': '#262335',
+      }
+    },
+    fontFamily: {
+      'fira-code': 'Fira Code, monospace',
+    },
   },
   variants: {
     extend: {},
   },
   plugins: [],
-}
+};
